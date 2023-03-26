88fb6b72d0813c559fe208d3cb97f63813c37819
yuri3 <48235548+Yuri3-xr@users.noreply.github.com>
2023-03-26 16:17:56 +0800

    more detail on ds

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,29 @@
 
 using i64 = std::int64_t;
 
+constexpr int INF = std::numeric_limits<int>::max() / 2.0;
+constexpr uint32_t SEED = 229;
+
+std::mt19937 seed(SEED);
+
+inline int randomInt(int l, int r) {
+    // return Int [l,r);
+    std::uniform_int_distribution<int> RNG(l, r + 1);
+    return RNG(seed);
+}
+
 struct Edge {
     int id;  // 边的标号
+    int from;
     int to;
     int distance;                  // 距离
     std::vector<int> markChannel;  //  表示i信道被markChannel[i]任务是用了
     int cntChannel;                // 记录当前剩余的信道数量
     Edge() = delete;
     Edge(int P) : markChannel(P, -1), cntChannel(P){};
-    Edge(int to, int distance, int P, int id)
-        : to(to),
+    Edge(int from, int to, int distance, int P, int id)
+        : from(from),
+          to(to),
           distance(distance),
           markChannel(P, -1),
           cntChannel(P),
@@ -20,14 +33,29 @@ struct Edge {
 
 struct Graph {
     std::vector<std::vector<Edge>> adj;
+    std::vector<std::vector<int>> mat;
+    std::vector<std::pair<Edge, std::pair<int, int>>>
+        edgeSet;  // 第二个pair表示u,v(u<v) {u的位置，v的位置}
     int P;
     int cnt = 0;
-    Graph(int n, int P) : adj(n), P(P){};
+    Graph(int n, int P) : adj(n), P(P), mat(n, std::vector<int>(n, INF)){};
     inline void addEdge(int from, int to, int d) {
-        adj[from].emplace_back(to, d, P, cnt++);
-        adj[to].emplace_back(from, d, P, cnt++);
+        adj[from].emplace_back(from, to, d, P, cnt);
+        adj[to].emplace_back(to, from, d, P, cnt);
+        mat[from][to] = mat[to][from] = std::min(mat[from][to], d);
+        ++cnt;
         return;
     };
+};
+
+struct Task {
+    int id;
+    int from, to;
+    int channel = -1;
+    std::vector<int> pathEdge = {};
+    std::vector<int> pathNode = {};
+    std::vector<int> station = {};
+    int totalSumChannel = 0;
 };
 
 /*
@@ -38,7 +66,34 @@ P: 信道上限
 D: 衰减上限
 */
 int N, M, T, P, D;
-std::vector<std::pair<int, int>> task;
+std::vector<Task> taskList;
+
+void outputAnswer(const Graph& G) {
+    std::cout << G.cnt - M << "\n";
+    for (int from = 0; from < N; ++from) {
+        for (auto e : G.adj[from]) {
+            int to = e.to;
+            if (e.id < M) continue;
+            if (from > to) continue;
+            std::cout << from << " " << to << "\n";
+        }
+    }
+
+    for (int i = 0; i < T; ++i) {
+        std::cout << taskList[i].channel << " "
+                  << (int)taskList[i].pathEdge.size() << " "
+                  << (int)taskList[i].station.size();
+        for (auto edgeId : taskList[i].pathEdge) {
+            std::cout << " " << edgeId;
+        }
+        for (auto stationId : taskList[i].station) {
+            std::cout << " " << stationId;
+        }
+        std::cout << "\n";
+    }
+
+    return;
+}
 
 int main() {
     std::ios::sync_with_stdio(false);
@@ -46,7 +101,6 @@ int main() {
 
     std::cin >> N >> M >> T >> P >> D;
     Graph G(N, P);
-
     for (int i = 0; i < M; i++) {
         int _u, _v, _d;
         std::cin >> _u >> _v >> _d;
@@ -56,7 +110,8 @@ int main() {
     for (int i = 0; i < T; i++) {
         int _from, _to;
         std::cin >> _from >> _to;
-        task.emplace_back(_from, _to);
+        taskList.push_back({i, _from, _to});
     }
+
     return 0;
 }
